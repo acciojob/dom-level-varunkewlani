1730386f6009d31e8e5194cd73b09f8544b0091a
varunkewlani <91694985+varunkewlani@users.noreply.github.com>
2024-08-10 17:23:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let abc=document.getElementById("level")
+count=1
+while (abc.previousElementSibling) 
+{
+	count++
+	abc=abc.previousElementSibling	
+}
+alert("The level of the element is: " + count)
